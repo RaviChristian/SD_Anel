1b384f44b6d0612bd93825cb9915a7cb876e6d31
Ravi Christian <108839457+RaviChristian@users.noreply.github.com>
2023-05-17 22:41:54 -0300

    Add files via upload

--- anel.py
+++ anel.py
@@ -6,7 +6,7 @@ id_lider = 3
 class Processo:
     def __init__(self, id):
         self.id = id
-        self.processos_indisponiveis = []  # Lista de processos indisponíveis
+        self.processos_indisponiveis = []
         self.ativo = True
 
     # Método para enviar uma mensagem para outro processo
@@ -17,13 +17,6 @@ class Processo:
         else:
             self.iniciar_eleicao()
             
-        # Implemente aqui a lógica de envio da mensagem ao processo destinatário
-        # Por exemplo, chamar o método receber_mensagem() do objeto do processo destinatário
-        # Certifique-se de tratar de forma adequada a exclusão mútua ao acessar a id_lider do destinatário
-
-        # Exemplo de implementação: chamar o método receber_mensagem() do objeto destinatário diretamente
-
-    # Método para receber uma mensagem de outro processo
 
     def iniciar_eleicao(self):
         global id_lider
@@ -34,15 +27,6 @@ class Processo:
         
         print(f"O novo processo líder é : Processo {id_lider}")
 
-#        if mensagem.startswith("eleicao:"):
-#            mensagem_parts = mensagem.split(":")
-#            if len(mensagem_parts) >= 2 and mensagem_parts[1]:
-#                id_recebido = int(mensagem_parts[1])
-
-#                if id_lider is None or id_recebido > id_lider:
-#                    id_lider = id_recebido
-#                    print(f"O novo processo líder é : Processo{id_lider}")
-#                    return
 
     def receber_mensagem(self, mensagem):
         global id_lider
@@ -60,9 +44,6 @@ num_processos = 5
 
 # Criar os objetos de processo
 processos = [Processo(id) for id in range(num_processos)]
-
-# Configurar processos indisponíveis (exemplo: processo 1 está indisponível)
-processos[1].processos_indisponiveis = [1]
 
 # Iniciar as threads para cada processo
 threads = []
@@ -84,12 +65,3 @@ processos[id_lider].ativo = False
 processos[processo_inicial].enviar_mensagem(processos[processo_inicial], f"Oi processo {processo_inicial}")
 print("lider atual: ",id_lider)
 
-
-
-
-# Definimos um líder inicial
-# Eleição vai iniciar quando: Processos cairem.
-# Caso o líder esteja inativo, faremos uma nova eleição.
-
-#Como vai funcionar a eleição : Percorreremos o array de processos, e todos mandarão seu id, o maior id será o novo líder.
-#Percorrer de trás pra frente, o maior id disponível sera o lider.
