44fc00adb187314279368a0f9bf33f3f585b7601
Ravi Christian <108839457+RaviChristian@users.noreply.github.com>
2023-05-17 22:40:42 -0300

    Add files via upload

--- anel.py
+++ anel.py
@@ -1,97 +1,95 @@
-import socket
+import threading
 
-# Define o endereço IP e a porta usada para a comunicação
-IP = '127.0.0.1'
-PORT = 5000
+id_lider = 3
 
-# Define a estrutura de dados para representar um processo
-class Process:
-    def __init__(self, id, ip, port):
+# Classe para representar um processo
+class Processo:
+    def __init__(self, id):
         self.id = id
-        self.ip = ip
-        self.port = port
-        self.leader = None  # armazena o id do processo líder
+        self.processos_indisponiveis = []  # Lista de processos indisponíveis
+        self.ativo = True
 
-        # Inicializa o socket TCP/IP para o processo
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.ip, self.port))
-        self.socket.listen(1)
-
-
-# Define uma lista encadeada circular de processos
-processes = [
-    Process(0, IP, PORT),
-    Process(1, IP, PORT + 1),
-    Process(2, IP, PORT + 2),
-    Process(3, IP, PORT + 3)
-]
-
-# Define a função para enviar uma mensagem para o próximo processo no anel
-def send_message(msg, next_process):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((next_process.ip, next_process.port))
-    sock.send(msg.encode())
-    sock.close()
-
-# Define a função para receber e processar uma mensagem recebida
-def process_message(msg, process):
-    if msg.startswith("original"):
-        next_process = processes[(process.id + 1) % len(processes)]
-        send_message(msg, next_process)
-
-    elif msg.startswith("election"):
-        initiator_id = int(msg.split(":")[1])
-        if initiator_id > process.id:
-            # passa a mensagem de eleição para o próximo processo no anel
-            next_process = processes[(process.id + 1) % len(processes)]
-            send_message(msg, next_process)
-        elif initiator_id < process.id:
-            # se o número do processo iniciador for menor, se declara como líder
-            process.leader = process.id
-            # envia mensagem de confirmação para o próximo processo no anel
-            leader_msg = f"leader:{process.id}"
-            next_process = processes[(process.id + 1) % len(processes)]
-            send_message(leader_msg, next_process)
+    # Método para enviar uma mensagem para outro processo
+    def enviar_mensagem(self, destinatario, mensagem):
+        print(f"Processo {self.id}: Enviando mensagem para o processo {destinatario.id}: {mensagem}")
+        if processos[id_lider].ativo == True:
+            destinatario.receber_mensagem(mensagem)
         else:
-            # o processo iniciador completou a volta no anel, se declara como líder
-            process.leader = process.id
-            # envia mensagem de confirmação para todos os outros processos no anel
-            for p in processes:
-                if p.id != process.id:
-                    leader_msg = f"leader:{process.id}"
-                    send_message(leader_msg, p)
+            self.iniciar_eleicao()
+            
+        # Implemente aqui a lógica de envio da mensagem ao processo destinatário
+        # Por exemplo, chamar o método receber_mensagem() do objeto do processo destinatário
+        # Certifique-se de tratar de forma adequada a exclusão mútua ao acessar a id_lider do destinatário
 
-    elif msg.startswith("leader"):
-        leader_id = int(msg.split(":")[1])
-        if leader_id == process.id:
-            # processo reconhece que é o líder
-            process.leader = process.id
-        else:
-            # passa a mensagem de confirmação para o próximo processo no anel
-            next_process = processes[(process.id + 1) % len(processes)]
-            send_message(msg, next_process)
+        # Exemplo de implementação: chamar o método receber_mensagem() do objeto destinatário diretamente
 
-    def send_election_message(next_process):
-        msg = f"ELECTION {process.id}"
-        send_message(msg, next_process)
-# Define o loop principal do processo
-while True:
-    # Aguarda a chegada de uma conexão de entrada
-    conn, addr = s.accept()
+    # Método para receber uma mensagem de outro processo
 
-    # Recebe a mensagem enviada pelo processo vizinho
-    msg = conn.recv(1024).decode()
+    def iniciar_eleicao(self):
+        global id_lider
 
-    # Processa a mensagem recebida
-    process_message(msg, processes[0])
-    # Envia mensagem de eleição para o primeiro processo no anel
-    send_election_message(processes[1])
+        for processo in processos:
+            if processo.ativo == True and processo.id > id_lider:
+                id_lider = processo.id
+        
+        print(f"O novo processo líder é : Processo {id_lider}")
 
-    # Aguarda até que o líder seja definido
-    while True:
-        if processes[0].leader is not None:
-            print(f"Processo {processes[0].id} é o líder.")
-            break
+#        if mensagem.startswith("eleicao:"):
+#            mensagem_parts = mensagem.split(":")
+#            if len(mensagem_parts) >= 2 and mensagem_parts[1]:
+#                id_recebido = int(mensagem_parts[1])
 
-    # Fecha a conexão de entrada
-    conn.close()
+#                if id_lider is None or id_recebido > id_lider:
+#                    id_lider = id_recebido
+#                    print(f"O novo processo líder é : Processo{id_lider}")
+#                    return
+
+    def receber_mensagem(self, mensagem):
+        global id_lider
+
+        if mensagem == "Iniciando Thread":
+            print(f"Processo {self.id}: Iniciando Thread \n")
+            return
+        
+        print(f"Processo {self.id}: Recebendo mensagem: {mensagem} \n")
+
+
+
+# Número de processos no anel
+num_processos = 5
+
+# Criar os objetos de processo
+processos = [Processo(id) for id in range(num_processos)]
+
+# Configurar processos indisponíveis (exemplo: processo 1 está indisponível)
+processos[1].processos_indisponiveis = [1]
+
+# Iniciar as threads para cada processo
+threads = []
+for processo in processos:
+    thread = threading.Thread(target=processo.receber_mensagem, args=("Iniciando Thread",))
+    thread.start()
+    threads.append(thread)
+
+# Aguardar o término de todas as threads
+for thread in threads:
+    thread.join()
+
+# Iniciar o algoritmo de eleição a partir de um processo
+processo_inicial = 2
+print("lider atual: ",id_lider)
+processos[processo_inicial].enviar_mensagem(processos[processo_inicial], f"Oi processo {processo_inicial}")
+
+processos[id_lider].ativo = False
+processos[processo_inicial].enviar_mensagem(processos[processo_inicial], f"Oi processo {processo_inicial}")
+print("lider atual: ",id_lider)
+
+
+
+
+# Definimos um líder inicial
+# Eleição vai iniciar quando: Processos cairem.
+# Caso o líder esteja inativo, faremos uma nova eleição.
+
+#Como vai funcionar a eleição : Percorreremos o array de processos, e todos mandarão seu id, o maior id será o novo líder.
+#Percorrer de trás pra frente, o maior id disponível sera o lider.
