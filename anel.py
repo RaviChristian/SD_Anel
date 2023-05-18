f55d20c640e179597fef345004ce3ce7e3e109f5
Ravi Christian <108839457+RaviChristian@users.noreply.github.com>
2023-05-17 23:35:03 -0300

    Add files via upload

--- anel.py
+++ anel.py
@@ -1,4 +1,5 @@
 import threading
+import time
 
 id_lider = 3
 
@@ -11,10 +12,13 @@ class Processo:
 
     # Método para enviar uma mensagem para outro processo
     def enviar_mensagem(self, destinatario, mensagem):
+        time.sleep(0.5)
         print(f"Processo {self.id}: Enviando mensagem para o processo {destinatario.id}: {mensagem}")
         if processos[id_lider].ativo == True:
             destinatario.receber_mensagem(mensagem)
         else:
+            print(f"O processo {destinatario.id} não pode receber a mensagem, pois o líder está inativo.")
+            print("Iniciando uma nova eleição de líder \n")
             self.iniciar_eleicao()
             
 
@@ -25,14 +29,15 @@ class Processo:
             if processo.ativo == True and processo.id > id_lider:
                 id_lider = processo.id
         
-        print(f"O novo processo líder é : Processo {id_lider}")
+        print(f"O novo processo líder é : Processo {id_lider}\n")
 
 
     def receber_mensagem(self, mensagem):
+        time.sleep(0.5)
         global id_lider
 
         if mensagem == "Iniciando Thread":
-            print(f"Processo {self.id}: Iniciando Thread \n")
+            print(f"Processo {self.id}: Iniciando Thread")
             return
         
         print(f"Processo {self.id}: Recebendo mensagem: {mensagem} \n")
@@ -51,17 +56,27 @@ for processo in processos:
     thread = threading.Thread(target=processo.receber_mensagem, args=("Iniciando Thread",))
     thread.start()
     threads.append(thread)
+    time.sleep(0.5)
 
-# Aguardar o término de todas as threads
-for thread in threads:
-    thread.join()
-
+print("\n")
+time.sleep(1)
 # Iniciar o algoritmo de eleição a partir de um processo
-processo_inicial = 2
-print("lider atual: ",id_lider)
-processos[processo_inicial].enviar_mensagem(processos[processo_inicial], f"Oi processo {processo_inicial}")
+processo_envia = 2
+processo_recebe = 4
+#print("lider atual: ",id_lider)
+processos[processo_envia].enviar_mensagem(processos[processo_recebe], f"Oi processo {processo_recebe}")
 
 processos[id_lider].ativo = False
-processos[processo_inicial].enviar_mensagem(processos[processo_inicial], f"Oi processo {processo_inicial}")
-print("lider atual: ",id_lider)
 
+processo_envia = 2
+processo_recebe = 1
+
+processos[processo_envia].enviar_mensagem(processos[processo_recebe], f"Oi processo {processo_recebe}")
+#print("lider atual: ",id_lider)
+
+
+processo_envia = 3
+processo_recebe = 2
+
+processos[processo_envia].enviar_mensagem(processos[processo_recebe], f"Oi processo {processo_recebe}")
+#print("lider atual: ",id_lider)
